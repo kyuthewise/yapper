19950e75ce99d9e0a52825a0905d011c97f7b78f
kyuthewise <91840641+kyuthewise@users.noreply.github.com>
2023-12-25 03:16:08 +0100

    dD

--- server/server.ts
+++ server/server.ts
@@ -71,7 +71,7 @@ socket.on('login', (userid) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; 
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
