a0121c832ee449e35df59025f0b1cf4f6f5995d1
kyuthewise <91840641+kyuthewise@users.noreply.github.com>
2023-12-25 03:00:51 +0100

    mt

--- app/lib/server.ts
+++ app/lib/server.ts
@@ -4,7 +4,7 @@ import Create from 'next-connect';
 export const connectMongoDB = async () =>{
     try{
         if(process.env.MONGODB_URL)
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect('mongodb+srv://kyujook:redert77@cluster0.kzjq4e6.mongodb.net/?retryWrites=true&w=majority')
         console.log('connected mongodb')
     }
     catch(error){
