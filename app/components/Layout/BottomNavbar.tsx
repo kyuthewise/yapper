c8b6723e2d8f58c2459762642064a700f4f972a4
kyuthewise <91840641+kyuthewise@users.noreply.github.com>
2023-12-30 12:41:36 +0100

    botnavbarimp

--- app/components/Layout/BottomNavbar.tsx
+++ app/components/Layout/BottomNavbar.tsx
@@ -11,20 +11,20 @@ const BottomNavbar = ({ toggleUserInfo, toggleFriendList, setDisableInterface}) 
   }
   return (
     <div className="bg-white dark:bg-gray-700 p-3 fixed bottom-0 inset-x-0 z-50 w-full flex justify-around items-center border-t border-gray-200 dark:border-gray-700 lg:hidden dark:text-gray-300">
-      <button onClick={toggleUserInfo} className="flex flex-col items-center space-y-1">
-        <UserIcon className="h-6 w-6 text-gray-700 dark:text-gray-300" />
-        <span className="text-xs text-gray-700 dark:text-gray-300">Profile</span>
-      </button>
-      <button onClick={toggleFriendList} className="flex flex-col items-center space-y-1">
-        <UsersIcon className="h-6 w-6 text-gray-700 dark:text-gray-300" />
-        <span className="text-xs text-gray-700 dark:text-gray-300">Friends</span>
-      </button>
-      <Link href="/dashboard" onClick={handleClick}>
+   <Link href="/dashboard" onClick={handleClick}>
         <div className='flex flex-col items-center space-y-1'>
         <HomeIcon className='h-6 w-6 text-gray-700 dark:text-gray-300'/>
         <span className="text-xs text-gray-700 dark:text-gray-300">Home</span>
         </div>
       </Link>
+      <button onClick={toggleFriendList} className="flex flex-col items-center space-y-1">
+        <UsersIcon className="h-6 w-6 text-gray-700 dark:text-gray-300" />
+        <span className="text-xs text-gray-700 dark:text-gray-300">Friends</span>
+      </button>
+      <button onClick={toggleUserInfo} className="flex flex-col items-center space-y-1">
+        <UserIcon className="h-6 w-6 text-gray-700 dark:text-gray-300" />
+        <span className="text-xs text-gray-700 dark:text-gray-300">Profile</span>
+      </button>
     </div>
   );
 };
